9fee2ea92eee60e370d0e39bc6acc9fb7bcf563a
Anastasia Turukina <turuk2021nas@mail.ru>
2025-04-13 23:34:15 +0700

    Initial commit

---
+++ Number_1.py
@@ -0,0 +1,17 @@
+class BigBell:
+    def __init__(self):
+        self.is_ding = True
+
+    def sound(self):
+        if self.is_ding:
+            print("ding")
+        else:
+            print("dong")
+        self.is_ding = not self.is_ding
+
+# Пример использования
+bell = BigBell()
+bell.sound()  # Вывод: ding
+bell.sound()  # Вывод: dong
+bell.sound()  # Вывод: ding
+bell.sound()  # Вывод: dong
